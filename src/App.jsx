b0f03d5750240671f321ca63df9667c1cf0163f0
jass2k5 <jsssndl@gmail.com>
2026-06-13 18:44:02 +0530

    all ready to work now

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export const App = ()=>{
+  return(
+    <div className="h-screen w-screen bg-red-400 text-white flex justify-center items-center">
+      <h1>hope it works this time dawg</h1>
+    </div>
+  )
+}
