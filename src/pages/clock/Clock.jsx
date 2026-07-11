40ace0b638a7e16be35f4eded311c93567a42b33
jass2k5 <jsssndl@gmail.com>
2026-07-12 00:02:20 +0530

    clock ui for night done

--- src/pages/clock/Clock.jsx
+++ src/pages/clock/Clock.jsx
@@ -15,6 +15,40 @@ export const Clock = () => {
     const telemetryData = useOsStore((state) => state.telemetryData);
     const temperature = telemetryData?.current?.temp_c ?? 0;
     const weatherText = telemetryData?.current?.condition?.text ?? "Couldn't fetch the data";
+    const feelsLike = telemetryData?.current?.feelslike_c ?? "--";
+    const humidity = telemetryData?.current?.humidity ?? "--";
+    const windSpeed = telemetryData?.current?.wind_kph ?? "--";
+    const visibility = telemetryData?.current?.vis_km ?? "--";
+    const [liveDate, setLiveDate] = useState(null);
+   
+
+    useEffect(() => {
+        let timer;
+        if (telemetryData?.location?.localtime) {
+            const apiTimeString = telemetryData.location.localtime.replace(/-/g, "/");
+            let currentDate = new Date(apiTimeString);
+
+            setLiveDate(currentDate);
+
+            timer = setInterval(() => {
+                currentDate = new Date(currentDate.getTime() + 1000);
+                setLiveDate(currentDate);
+            }, 1000);
+        }
+        return () => clearInterval(timer);
+    }, [telemetryData]);
+
+    if(!liveDate) return;
+     const timestring = liveDate.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit"
+    });
+
+    const dayName = liveDate.toLocaleDateString("en-US", { weekday: "short" });
+    const monthName = liveDate.toLocaleDateString("en-US", { month: "short" });
+    const dateNum = liveDate.getDate();
+    const year = liveDate.getFullYear();
+
     return (
         <div style={{ zIndex: myZIndex }} className="Clock h-full w-full absolute top-0 left-0 bg-black p-10 flex items-center justify-center">
             <div className="h-[70vh] w-[70vw] overflow-y-scrol scrollbar-none relative rounded-3xl">
@@ -23,8 +57,34 @@ export const Clock = () => {
                     <div className="topLeft h-auto w-auto bg-transparent absolute top-[4%] left-[4%] z-10 flex flex-col justify-center items-start gap-0.5">
                         <span className={`countrySpan uppercase text-1xl font-medium ${isDay ? "text-gray-600 " : "text-white/60"}`}><i className="ri-map-pin-line"></i> {telemetryData?.location?.country}</span>
                         <span className="citySpan uppercase text-7xl font-[Lora]">{telemetryData?.location?.name}</span>
-                        <span className={`conditionSpan uppercase text-1xl ${isDay ? "text-gray-600 ": "text-white/60"}`}>{isDay?<i class="ri-sun-line"></i>:<i class="ri-moon-line"></i>}  {weatherText}</span>
+                        <span className={`conditionSpan uppercase text-1xl ${isDay ? "text-gray-600 " : "text-white/60"}`}>{isDay ? <i className="ri-sun-line"></i> : <i className="ri-moon-line"></i>}  {weatherText}</span>
                     </div>
+                    <div className="bottomLeft absolute bottom-[6%] left-[3%] flex flex-col items-start justify-center">
+                        <span className={`temp text-8xl  font-[Lora] drop-shadow-2xl ${isDay ? "text-gray-600 " : "text-white"}`}>{temperature}°C</span>
+                    </div>
+                    <div className="dataTime absolute top-[5%] right-[3%] flex flex-col justify-center items-center ">
+                        <span className="date capitalize text-xl font-[Lora]">{monthName} {dateNum},  {year}</span>
+                        <span className="self-end">{timestring}</span>
+                    </div>
+
+                    <div className="bottomRight absolute bottom-[5%] right-[4%] flex justify-center items-center gap-3.5">
+                        <div className="feels flex flex-col justify-start items-start gap-1 border-2 border-white/20 rounded-xl pr-6 pl-4 pt-2 pb-2  bg-white/10 backdrop-blur-2xl">
+                            <i className="ri-temp-hot-line capitalize text-[14px] text-white/50 "> feels</i>
+                            <span>{feelsLike} °C</span>
+                        </div>
+                        <div className="humidity flex flex-col justify-start items-start gap-1 border-2 border-white/20 rounded-xl pr-6 pl-4 pt-2 pb-2 bg-white/10 backdrop-blur-2xl">
+                            <i className="ri-water-percent-line capitalize text-[14px] text-white/50 "> humidity</i>
+                            <span>{humidity}%</span>
+                        </div>
+                        <div className="wind flex flex-col justify-start items-start gap-1 border-2 border-white/20 rounded-xl pr-6 pl-4 pt-2 pb-2 bg-white/10 backdrop-blur-2xl">
+                            <i className="ri-cloud-windy-line capitalize text-[14px] text-white/50 "> wind</i>
+                            <span>{windSpeed} Km/h</span>
+                        </div>
+                        <div className="wind flex flex-col justify-start items-start gap-1 border-2 border-white/20 rounded-xl pr-6 pl-4 pt-2 pb-2 bg-white/10 backdrop-blur-2xl">
+                            <i className="ri-eye-line capitalize text-[14px] text-white/50 "> wind</i>
+                            <span>{visibility} Km</span>
+                        </div>
+                    </div> 
 
                 </div>
             </div>
