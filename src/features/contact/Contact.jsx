160f4ae0872f7142142362ca54d30a9fbc0f662b
jass2k5 <jsssndl@gmail.com>
2026-07-29 11:59:53 +0530

    feat:added lenis smooth scroller to contact App

--- src/features/contact/Contact.jsx
+++ src/features/contact/Contact.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { useOsStore } from '../../shared/store/useOsStore';
+import Lenis from 'lenis'; 
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 import { Header } from './header/Header';
 import { Form } from './form/Form';
 import { Information } from './information/Information';
 import { Socials } from './socials/Socials';
 import { Footer } from './footer/Footer';
 import { CenterImage } from './information/CenterImage';
+
+gsap.registerPlugin(ScrollTrigger);
 export const ContactApp = () => {
     const addNotification = useOsStore((state) => state.addNotification);
     const Zindex = useOsStore((state) => 10 + state.windowOrder.indexOf("contact"));
+    const scrollContainerRef = useRef(null);
+    useEffect(() => {
+        const lenis = new Lenis({
+            wrapper: scrollContainerRef.current, 
+            content: scrollContainerRef.current.querySelector('.contactMain'),
+            duration: 1,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        });
 
+      
+        lenis.on('scroll', ScrollTrigger.update);
+        gsap.ticker.add((time) => {
+            lenis.raf(time * 1000);
+        });
+        gsap.ticker.lagSmoothing(0, 0);
+
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
 
     return (
         <div style={{
@@ -18,7 +42,7 @@ export const ContactApp = () => {
             containerName: 'contactApp'
         }}
             className='w-full h-full absolute inset-0'>
-            <div className='w-full h-full overflow-y-auto overflow-x-hidden custom-scrollbar relative'>
+            <div ref={scrollContainerRef} className='w-full h-full overflow-y-auto overflow-x-hidden custom-scrollbar relative'>
                 <main className='contactMain'>
                     <Header />
                     <Form />
