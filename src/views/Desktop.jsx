524bcf59a5c7012fbbf89be850129491685fde6e
jass2k5 <jsssndl@gmail.com>
2026-08-03 15:00:27 +0530

    Feat:created keyboard btn in desktop to show shortcuts and eventlisterner click anywhere and pointer event none

--- src/views/Desktop.jsx
+++ src/views/Desktop.jsx
@@ -1,4 +1,4 @@
-import { useEffect, lazy, Suspense } from 'react';
+import { useEffect, lazy, Suspense, useState, useRef } from 'react';
 import { useQueryClient } from "@tanstack/react-query";
 import { useOsStore } from "../shared/store/useOsStore";
 import { DraggableWindow } from "../shared/components/DraggableWindow";
@@ -9,6 +9,16 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { MapCrashFallback } from "../features/map/ErrorBoundaryMap";
 import { useSyncAllWeather } from "../shared/hooks/useSyncAllWeather";
 import { useKeys } from '../shared/hooks/useKeys';
+//assets
+import keyword from '../shared/assets/keyword.png';
+import alt from '../shared/assets/Alt.png'
+import back from '../shared/assets/backbtn.png'
+import one from '../shared/assets/1.png'
+import two from '../shared/assets/2.png'
+import three from '../shared/assets/3.png'
+import four from '../shared/assets/4.png'
+import five from '../shared/assets/5.png'
+import six from '../shared/assets/6.png'
 // Lazy Loaded Apps
 const WeatherMap = lazy(() => import("../features/map/Map").then(m => ({ default: m.WeatherMap })));
 const Clock = lazy(() => import("../features/clock/Clock").then(m => ({ default: m.Clock })));
@@ -24,9 +34,21 @@ const APP_CONFIG = [
         { id: 'clock', title: "Clock", Component: Clock, minW: 459, minH: 406, defW: window.innerWidth, defH: window.innerHeight, posX: 0, posY: 0 }
     ];
 
+const shortCuts =[
+    {app:"map",src:one},
+    {app:"dashboard",src:two},
+    {app:"clock",src:three},
+    {app:"contact",src:four},
+    {app:"notify",src:five},
+    {app:"setting",src:six},
+    {app:"closeAll",src:back}
+]
+
 
 export const Desktop = () => {
     const queryClient = useQueryClient();
+    const [isOpen,setIsOpen] = useState(false);
+    const shortCutRef = useRef(null);
     const bgUrl = useOsStore((state) => state.systemBg);
     const apps = useOsStore((state) => state.apps);
     const addNotification = useOsStore((state) => state.addNotification);
@@ -48,7 +70,18 @@ export const Desktop = () => {
             addNotification("Synced All Weather Data. Will Update after 15 minutes", "info");
         }, 4000);
         
-        return () => clearTimeout(timer); 
+        const handleOutsideClick = (e)=>{
+            if(shortCutRef.current && !shortCutRef.current.contains(e.target)){
+                setIsOpen(false);
+            }
+        }
+
+        window.addEventListener("mousedown",handleOutsideClick)
+        
+        return () => {
+            clearTimeout(timer);
+            window.removeEventListener("mousedown",handleOutsideClick);
+        } 
     }, []);
 
 
@@ -58,6 +91,31 @@ export const Desktop = () => {
             style={{ backgroundImage: `url('${bgUrl}')` }} 
             className="desktop h-screen w-screen relative bg-cover bg-center bg-no-repeat overflow-hidden"
         >
+           <div ref={shortCutRef} className='shortcut select-none  z-15 h-max w-max absolute right-[1%] bottom-[3%] p-2 flex flex-col justify-center items-center gap-5 pointer-events-none'>
+
+            <div ref={shortCutRef} className={`guide h-max w-max p-2 bg-white border border-white/60 rounded-2xl ${!isOpen?'opacity-0':'opacity-100'}`}>
+             {shortCuts.map((short,index)=>(
+                <div className='h-max w-max p-1 flex gap-2'>
+                    <img className='h-8' src={alt} alt="alt"/>
+                    <span className='text-xl text-black'>+</span>
+                    <img className='h-8' src={short.src} alt="Number" />
+                    <i class="ri-arrow-right-line text-black pt-1"></i>
+                    <span className='text-black text-[1rem]'>Toggle {short.app}</span>
+                </div>
+             ))}
+            </div>
+
+             <button
+              onClick={()=>{
+                if(isOpen){
+                    setIsOpen(false);
+                }else{
+                    setIsOpen(true);
+                }
+              }}
+              className='pointer-events-auto cursor-pointer active:scale-[0.96]'> <img src={keyword} alt="keyword" /></button>
+           </div>
+
             {glassSettings?.enabled && (
                 <div style={{
                     backdropFilter: `blur(${glassSettings.blurValue || 0}px)`,
