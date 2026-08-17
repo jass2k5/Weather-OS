9c386a1bd26790ed093f5ea9b7b947f118f65a23
jass2k5 <jsssndl@gmail.com>
2026-08-18 02:00:44 +0530

    New Vite_env fiile to avoid env error by typescript and converted usecityforecast too

---
+++ src/shared/hooks/useCityForecast.ts
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export const useCityForecast = (cityname:string) => {
+    const Api_Key = import.meta.env.VITE_WEATHER_API_KEY;
+    const BASE_URL = 'https://api.weatherapi.com/v1';
+
+    return useQuery({
+        queryKey: ["forecast", cityname],
+        queryFn: async () => {
+            const response = await axios.get(`${BASE_URL}/forecast.json`, {
+                params: {
+                    key: Api_Key,
+                    q: cityname,
+                    days: 7,
+                    aqi: "yes",
+                    alerts: "no"
+                }
+            });
+            return response.data;
+        },
+        staleTime: 1000 * 60 * 20,
+        refetchInterval: 1000 * 60 * 20,
+        enabled: !!cityname,
+    });
+}
