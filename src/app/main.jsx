edd1f2be36b62983a3e3a0b135ade98a8bcf2ad6
jass2k5 <jsssndl@gmail.com>
2026-08-20 02:25:09 +0530

    Added react query devtools for better checking of api cache

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -4,12 +4,14 @@ import { App } from './App'
 import 'remixicon/fonts/remixicon.css'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
 
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
 
 )
